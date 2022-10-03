40410e2ca841c741b1cd77fa9367704b0e2ef5cd
Travis Miller <milletr@tulsaschools.org>
2022-10-03 16:07:47 -0500

    Remove unnecessary comments and code

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,21 +1,9 @@
-// const HttpBackend = require('i18next-http-backend/cjs')
-// const ChainedBackend = require('i18next-chained-backend').default
-// const LocalStorageBackend = require('i18next-localstorage-backend').default
-
 const debug = process.env.NODE_ENV === 'development'
-// const expirationTime = debug ? 1 : 60 * 60 * 1000 // 1 hour
-// const undefinedWindow = typeof window === 'undefined'
 
 module.exports = {
   debug,
-  // backend: {
-  //   backendOptions: [{ expirationTime }, { /* loadPath: 'https:// somewhere else' */ }],
-  //   backends: undefinedWindow ? [] : [LocalStorageBackend, HttpBackend],
-  // },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
   },
-  // serializeConfig: false,
-  // use: undefinedWindow ? [] : [ChainedBackend],
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,8 @@ import React from 'react'
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation, useTranslation } from 'next-i18next'
 import nextI18nConfig from '../next-i18next.config'
-import { useTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }) {
   const { ready } = useTranslation()
