9ee7d4c836581de9b0930dfdb72752cdb5592ce9
Travis Miller <milletr@tulsaschools.org>
2022-12-02 02:02:28 -0600

    Update landing page

--- pages/[locale]/index.js
+++ pages/[locale]/index.js
@@ -8,9 +8,12 @@ export { getStaticPaths, getStaticProps }
 export default function Home() {
   return (
     <>
-      <h1 className={'text-center'}><Trans>Retention Incentive Calculator</Trans></h1>
+      <h1 className={'text-center'}><Trans>New Hire and Retention Incentive Calculators</Trans></h1>
 
       <div className={'flex justify-center space-x-2'}>
+        <Button href="/new-hire">
+          <Trans>New Hire for 22-23</Trans>
+        </Button>
         <Button href="/returning">
           <Trans>Returning for 22-23</Trans>
         </Button>
